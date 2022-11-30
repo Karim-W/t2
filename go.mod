74f2995bc582ce79f1e089b38f3c934ad89b436f
Karim <karim.wael@gmail.com>
2022-11-30 19:59:42 +0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karim-w/t2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func main() {
+	arg := os.Args[1]
+	//get substring from last / to end
+	lastIndex := strings.LastIndex(arg, "/")
+	last := arg[lastIndex+1:]
+	dir := arg[:lastIndex]
+	//split dir by / and check if each dir is a valid dir
+	dirs := strings.Split(dir, "/")
+	path := ""
+	for _, d := range dirs {
+		path += d + "/"
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			//create dir
+			os.Mkdir(path, 0777)
+		}
+	}
+	//check if last contains .
+	if strings.Contains(last, ".") {
+		//create file
+		f, err := os.Create(arg)
+		if err != nil {
+			os.Exit(1)
+		}
+		f.Close()
+
+	} else {
+		path += last + "/"
+		os.Mkdir(path, 0777)
+	}
+}
